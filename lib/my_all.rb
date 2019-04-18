ac83b457b1b74e322b56a47744a4b95bcf3957a8
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-18 14:58:26 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,14 @@ require 'pry'
 
 def my_all?(collection)
   array_index = 0
-  matching_values = []
+  bool_values = []
   while array_index < collection.length
-    
+    bool_values << yield(collection[array_index])
     array_index += 1
+  end
+  if bool_values.include?(false)
+    false
+  else
+    true
   end
 end
